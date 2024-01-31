51652c79192715418082c6afa3e2a2566aa5af3e
saudgul <158152703+Zeshan0075@users.noreply.github.com>
2024-01-31 16:44:48 +0500

    Import components

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../components/header/Header'
+import HeroSection from '../components/home/HeroSection'
 
 export default function LandingPage() {
   return (
     <div>
-      
+      <Header/>
+      <HeroSection/>
     </div>
   )
 }
