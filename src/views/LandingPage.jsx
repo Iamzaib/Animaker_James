dae812d9c09496e229ee3a4a953308da1f9a6442
saudgul <158152703+Zeshan0075@users.noreply.github.com>
2024-02-01 15:49:36 +0500

    add tools section and template section

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 import Header from '../components/header/Header'
 import HeroSection from '../components/home/HeroSection'
 import Awards from '../components/home/Awards'
+import Template from '../components/home/Template'
+import EditingTools from '../components/home/EditingTools';
 
 export default function LandingPage() {
   return (
-    <div>
-      {/* <Header/> */}
-      <HeroSection/>
-      <Awards/>
-    </div>
+    <>
+      <HeroSection />
+
+      {/* This div is for wrap the other component */}
+      <div className='bg-zinc-200'>
+        <div className='lg:mx-36'> 
+          <Awards />
+          <Template />
+          <EditingTools/>
+
+        </div>
+
+      </div>
+
+    </>
   )
 }
