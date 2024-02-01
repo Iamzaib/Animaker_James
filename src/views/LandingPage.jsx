d8be5fdfcd91528692d3ebe39053dee46bbf5390
Gulfam619 <157134864+Gulfam619@users.noreply.github.com>
2024-02-01 19:08:59 +0500

    Changes in WhyAnimaker and  TopProducts

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -4,18 +4,21 @@ import HeroSection from '../components/home/HeroSection'
 import Awards from '../components/home/Awards'
 import Template from '../components/home/Template'
 import EditingTools from '../components/home/EditingTools';
+import WhyAnimaker from '../components/home/WhyAnimaker'
+import TopProductsSection from '../components/home/TopProductsSection'
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
-
       {/* This div is for wrap the other component */}
       <div className='bg-zinc-200'>
         <div className='lg:mx-36'> 
           <Awards />
+          <WhyAnimaker />
           <Template />
           <EditingTools/>
+          <TopProductsSection />
 
         </div>
 
