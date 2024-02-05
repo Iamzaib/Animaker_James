8b0ea1b4497e6573865f6130a240e935b743609d
Gulfam619 <157134864+Gulfam619@users.noreply.github.com>
2024-02-05 10:18:25 +0500

    changes in landing Page

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -7,7 +7,7 @@ import EditingTools from '../components/home/EditingTools';
 import FaqSection from '../components/home/Faq'
 import Reviews from '../components/home/Reviews'
 import WhyAnimaker from '../components/home/WhyAnimaker'
-// import TopProductsSection from '../components/home/TopProductsSection'
+import TopProductsSection from '../components/home/TopProductsSection'
 import FirstVideo from '../components/home/FirstVideo'
 import SliderSection from '../components/home/SliderSection'
 
@@ -23,6 +23,7 @@ export default function LandingPage() {
           <WhyAnimaker />
           <Template />
           <EditingTools />
+          <SliderSection />
           <TopProductsSection />
           <Reviews/>
           <FaqSection />
