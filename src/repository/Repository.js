68d431d2abcba08200152a33988c6f533c9dd876
saud823 <saud_gul@nextpak.org>
2024-02-09 18:33:16 +0500

    ïmage   server

--- src/repository/Repository.js
+++ src/repository/Repository.js
@@ -2,25 +2,21 @@
 import axios from "axios";
 
 const baseDomain = "http://192.168.18.84:8188/";
-
-const baseURL = `${baseDomain}`;
-
+export const baseURL = `${baseDomain}`;
 let axiosObj;
 axiosObj = axios.create({
     baseURL,
-    
-    
 });
-axiosObj.interceptors.request.use(
-    (config) => {
-        // Dynamically set the token for each request
-        const ls = JSON.parse(localStorage.getItem("authUser"));
-        const token = ls?.token || "";
-        config.headers.token = token;
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
+// axiosObj.interceptors.request.use(
+//     (config) => {
+//         // Dynamically set the token for each request
+//         const ls = JSON.parse(localStorage.getItem("authUser"));
+//         const token = ls?.token || "";
+//         config.headers.token = token;
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
 export default axiosObj;
