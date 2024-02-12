4bf3e0f262131bad6117bf67811d55352519dd9f
Muhammad Zeshan <142181053+Zeshan1011@users.noreply.github.com>
2024-02-11 21:53:35 -0800

    adjust space left in xl screen

--- src/components/home/Template.jsx
+++ src/components/home/Template.jsx
@@ -9,7 +9,7 @@ export default function Template() {
         <div className='mt-20'>
 
             {/* This div is for heading */}
-            <div className='md:w-[50%] md:relative top-48 left-10' data-aos="zoom-in-up" data-aos-duration="1000">
+            <div className='md:w-[50%] md:relative top-48 left-10 xl:left-20' data-aos="zoom-in-up" data-aos-duration="1000">
                 <h1 className='text-3xl font-bold px-4'>1000s of Unique Video Templates</h1>
                 <p className='px-4 text-gray-400 text-lg'>Pick from 1000s of templates! Use them to create videos in under 5 minutes!</p>
             </div>
