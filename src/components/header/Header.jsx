f3102d80b7e21fd50c8dc13101f5b36a25930ef1
Gulfam619 <157134864+Gulfam619@users.noreply.github.com>
2024-02-13 10:08:16 +0500

    New Changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -52,12 +52,8 @@
 //                 </div>
 //             </div>
 
-
-
 //             {/* Mobile menu for small and medium screens */}
 //             <div className='md:hidden bg-[#2727d0]'>
-
-
 
 //                 <div className='flex justify-between items-center py-2 px-3 fixed-top z-10 bg-[#2727d0]'>
 
@@ -81,7 +77,6 @@
 //                     )}
 //                 </div>
 
-
 //                 <ul
 //                     className={`fixed h-screen w-[300px] bg-white z-10 top-[70px] transition-all ease-in-out duration-700
 //             ${toggle ? 'left-0' : 'left-[-100%]'} px-8 py-8`}
@@ -104,7 +99,6 @@
 //                             Pricing
 //                         </li>
 
-
 //                         <div className='flex flex-col'>
 //                             <button className=' border-2 rounded border-[#ff8825] px-2 py-1 border-b-2 my-2 w-[100px]'>
 //                                 Login
@@ -124,15 +118,13 @@
 //     );
 // }
 
-
-
 // After Update new header
-import React, { useEffect, useState } from 'react';
-import { BiWorld } from 'react-icons/bi';
-import { AiOutlineMenu } from 'react-icons/ai';
+import React, { useEffect, useState } from "react";
+import { BiWorld } from "react-icons/bi";
+import { AiOutlineMenu } from "react-icons/ai";
 import { MdOutlineClose } from "react-icons/md";
-import logo from '../../assets/img/animaker-logo.png';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import logo from "../../assets/img/animaker-logo.png";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Header() {
     const [toggle, setToggle] = useState(false);
@@ -144,10 +136,10 @@ export default function Header() {
             setIsScrolled(scrollPosition > 50);
         };
 
-        window.addEventListener('scroll', handleScroll);
+        window.addEventListener("scroll", handleScroll);
 
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
@@ -155,9 +147,15 @@ export default function Header() {
         <div>
             {/* Large screens */}
             {/* <div className='bg-[#2727d0]  fixed-top hidden md:flex justify-between items-center text-white  md:px-20 lg:px-32 py-2 text-[16px]'> */}
-            <div className={`bg-${isScrolled ? 'white' : '#2727d0'} fixed-top hidden md:flex justify-between items-center text-${isScrolled ? 'black' : 'white'} md:px-20 lg:px-32 py-2 text-[16px]`}>
+            <div
+                className={`bg-${
+                    isScrolled ? "white" : "#2727d0"
+                } fixed-top hidden md:flex justify-between items-center text-${
+                    isScrolled ? "black" : "white"
+                } md:px-20 lg:px-32 py-2 text-[16px]`}
+            >
                 <div>
-                    <img src={logo} alt='logo' className='w-[180px]' />
+                    <img src={logo} alt="logo" className="w-[180px]" />
                 </div>
                 {/* <div>
                     <ul className='flex gap-x-5 font-semibold hover:cursor-pointer'>
@@ -167,7 +165,7 @@ export default function Header() {
                         <li className='hover:text-[#ffa56d] hover:scale-105 duration-200'>Pricing</li>
                     </ul>
                 </div> */}
-                <div className='flex gap-x-5'>
+                <div className="flex gap-x-5">
                     {/* <button className='px-3 py-1 border border-transparent hover:border-white rounded-full'>
                         Login
                     </button>
@@ -180,47 +178,39 @@ export default function Header() {
                         <BiWorld size={30} />
                     </button> */}
                     <Link to="/animate">
-
-                        <button className='bg-gradient-to-r from-[#ff6c10] to-[#ffc450] rounded-full px-3 py-1 font-bold'>
+                        <button className="bg-gradient-to-r from-[#ff6c10] to-[#ffc450] rounded-full px-3 py-1 font-bold">
                             Try for free
                         </button>
                     </Link>
                 </div>
             </div>
 
-
-
             {/* Mobile menu for small and medium screens */}
-            <div className='md:hidden bg-[#2727d0]'>
-
-
-
-                <div className='flex justify-between items-center py-2 px-3 fixed-top z-10 bg-[#2727d0]'>
-
+            <div className="md:hidden bg-[#2727d0]">
+                <div className="flex justify-between items-center py-2 px-3 fixed-top z-10 bg-[#2727d0]">
                     {/* This div is for logo */}
                     <div>
-                        <img src={logo} alt='logo' className='w-[180px]' />
+                        <img src={logo} alt="logo" className="w-[180px]" />
                     </div>
 
                     {toggle ? (
                         <MdOutlineClose
                             onClick={() => setToggle(!toggle)}
-                            className='shadow-sm text-white mr-2 hover:cursor-pointer transition-transform transform scale-95 duration-1000'
+                            className="shadow-sm text-white mr-2 hover:cursor-pointer transition-transform transform scale-95 duration-1000"
                             size={30}
                         />
                     ) : (
                         <AiOutlineMenu
                             onClick={() => setToggle(!toggle)}
-                            className='mr-2 shadow-sm hover:cursor-pointer transition-transform duration-1000 scale-95  text-white'
+                            className="mr-2 shadow-sm hover:cursor-pointer transition-transform duration-1000 scale-95  text-white"
                             size={30}
                         />
                     )}
                 </div>
 
-
                 <ul
                     className={`fixed h-1/2 w-[300px] bg-white z-10 top-[70px] transition-all ease-in-out duration-700
-            ${toggle ? 'left-0' : 'left-[-100%]'} px-8 py-8`}
+            ${toggle ? "left-0" : "left-[-100%]"} px-8 py-8`}
                 >
                     {/* <ul className='block items-center gap-x-11 flex-wrap'> */}
 
@@ -240,8 +230,7 @@ export default function Header() {
                             Pricing
                         </li> */}
 
-
-                    <div className='flex flex-col'>
+                    <div className="flex flex-col">
                         {/* <button className=' border-2 rounded border-[#ff8825] px-2 py-1 border-b-2 my-2 w-[100px]'>
                                 Login
                             </button>
@@ -253,8 +242,7 @@ export default function Header() {
                                 <BiWorld size={30} />
                             </button> */}
                         <Link to="/animate">
-
-                            <button className='bg-gradient-to-r from-[#ff6c10] to-[#ffc450] rounded-full px-3 py-1 font-bold'>
+                            <button className="bg-gradient-to-r from-[#ff6c10] to-[#ffc450] rounded-full px-3 py-1 font-bold">
                                 Try for free
                             </button>
                         </Link>
